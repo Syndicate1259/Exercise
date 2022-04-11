a789685b54208222e92b3a90a0d1358773c02259
Syndicate1259 <jose.sanchez849@gmail.com>
2022-04-11 00:18:01 -0700

    HTTP API endpoint complete

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-const express = require('express')
-const app = express()
+var http = require('http'); //Used for HTTP
 const port = 3000 //port number web app is hosted
 
-app.get('/', (req, res) => {  //.get() is for GET requests, '/' is the home page, req is the request, res is the response we will be doing
-  //res.send('Hello World!') //Respond with "Hello World" when in '/' or home page
-  res.json({ //.json returns a json object
-    message: "My name is Jose Sanchez"  //Created to specification
-  })
-})
+
+
+var app = http.createServer(function(req,res){  //HTTP implementation
+    var unix_epoch = Math.floor(new Date().getTime() / 1000) //unix epoch in seconds
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({
+      message: "My name is Jose Sanchez" ,
+      timestamp: unix_epoch
+    }));
+});
 
 app.listen(port, () => { //used to bind and listen the connections on the specified host and port
   console.log(`Example app listening on port ${port}`)
